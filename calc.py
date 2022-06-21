342f91a8f73261d33f9133c53356ad93bfa13522
Leong-Si <Leong-Si@users.noreply.github.com>
2022-06-21 19:51:26 -0400

    Magic!

--- calc.py
+++ calc.py
@@ -1,25 +1,60 @@
 import sys
 
-def calc(expr):
-    if isinstance(expr, int):
-        return expr
-    elif isinstance(expr, str):
-        # check if the expr is only digit, therefore an int in string form
-        # this can happen when calc() is first called from main()
-        if expr.isdigit():
-            return expr
+# Find the first valid expression
+def firstExpr(args):
+    if not args.startswith('('):
+        return args.split(maxsplit=1)[0]
+    else:
+        openParenCount = 1
+        closeParenCount = 0
+        loopIndex = 1
 
-        # removes leading and trailing whitespace
-        expr = expr.strip()
-        if expr.startswith('(') and expr.endswith(')'):
-            # removes parenthesis
-            expr = expr[1:-1]
-            return expr
-    #
+        while loopIndex < len(args) and openParenCount != closeParenCount:
+            arg = args[loopIndex]
+            if arg == '(':
+                openParenCount = openParenCount + 1
+            elif arg == ')':
+                closeParenCount = closeParenCount + 1
+            loopIndex = loopIndex + 1
+
+        return args[:loopIndex]
+
+# Assuming matching open and close parenthesis, and formatting are all correct
+def calc(expr):
+    if expr.isdigit():
+        return int(expr)
+    if expr.startswith('(') and expr.endswith(')'):
+        # removes outer parenthesis
+        expr = expr[1:-1]
+
+        # separate function name and the rest of the expression
+        (funcName, separator, funcArgs) = expr.partition(' ')
+        funcName = funcName.lower()
+
+        argsLen = 0
+        args = []
+
+        while argsLen < len(funcArgs):
+            resultArg = firstExpr(funcArgs[argsLen:])
+            args.append(resultArg)
+            argsLen = argsLen + len(resultArg) + 1  # +1 for the trailing whitespace
+
+        # optionally check args length
+
+        if funcName == 'add':
+            return calc(args[0]) + calc(args[1])
+        elif funcName == 'multiply':
+            return calc(args[0]) * calc(args[1])
+
+    # If code reaches here, optionally throw an error
 
 def main():
     if len(sys.argv) > 1:
-        print(calc(sys.argv[1]))
+        arg = sys.argv[1].strip()
+        if isinstance(arg, int):
+            print(arg)
+        elif isinstance(arg, str):
+            print(calc(arg))
 
     # Do nothing if there is not enough arguments
 
